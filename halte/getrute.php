61fbf516309d1c2588f856987734e867dc9b9134
nawibowo <nurcahyoaw@gmail.com>
2017-06-07 09:10:18 +0700

    ganti metode json enocde get rute dan halte

--- halte/getrute.php
+++ halte/getrute.php
@@ -4,7 +4,12 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $result = $kon->query("SELECT * FROM rute");
 $outp = array();
-$outp = $result->fetch_all(MYSQLI_ASSOC);
+// $outp = $result->fetch_all(MYSQLI_ASSOC);
 
+// echo json_encode($outp);
+
+while ($hasil = $result->fetch_assoc()) {
+    $outp[] = $hasil;
+}
 echo json_encode($outp);
  ?>
